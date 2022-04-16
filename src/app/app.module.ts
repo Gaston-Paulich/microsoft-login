c1e9ccd0a366d2b6eb8e3751831dcaeab0009d31
Gaston Paulich <gastopau@quilmes.com.ar>
2022-04-15 21:15:01 -0300

    Se agrega el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MsalModule, MsalService, MSAL_INSTANCE } from '@azure/msal-angular';
+import { IPublicClientApplication, PublicClientApplication } from '@azure/msal-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PublicPageComponent } from './public-page/public-page.component';
+import { RestrictedPageComponent } from './restricted-page/restricted-page.component';
+
+export function MSALInstanceFactory():IPublicClientApplication {
+  return new PublicClientApplication({
+    auth: {
+      clientId: '064f3bfc-6170-4a05-8e58-bfcd9541eda8',
+      redirectUri: 'http://localhost:4200'
+
+
+    }
+
+
+  })
+}
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicPageComponent,
+    RestrictedPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MsalModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory
+
+
+    },
+    MsalService
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
